1f4acbe40b4690a4ffeb057dd0ca88e2fa3d3152
alexeyand <alexey_and@mail.ru>
2025-11-25 02:48:16 +0300

    first commit

--- init.sh
+++ init.sh
@@ -35,7 +35,9 @@ USER_ARGS=(
 
 check_user_args(){
   for a in "${USER_ARGS[@]}"; do
-    (( $a )) || { read -p "Please provide ${a}" $a }
+    (( $a )) || {
+      read -p "Please provide ${a}" $a;
+    }
   done
 }
 
