7e74330a05ca6070345c8caea1c5b8576d9cb551
alexeyand <alexey_and@mail.ru>
2025-11-25 04:06:15 +0300

    first commit

--- init.sh
+++ init.sh
@@ -48,8 +48,8 @@ check_user_args(){
 
     (( ${#s} )) && [[ "$a" =~ URL ]] && { check_url_format "$s" || s=""; }
     (( ${#s} )) || {
-      v=$(printf '%s' "Please provide ${a}: " >&2; read x && printf '%s' "$x")
-      
+      s=$(printf '%s' "Please provide ${s}: " >&2; read x && printf '%s' "$x")
+
 #      while read -r line && [ "$1" != 1 ]
 #      while read -p "Please provide ${a}: " v && check_url_format "$v";
 #      read -p "Please provide ${a}: " $v;
