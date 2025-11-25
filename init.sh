fba89a26b2aa8225292eb74d47df3206fef8e6d1
alexeyand <alexey_and@mail.ru>
2025-11-25 03:04:53 +0300

    first commit

--- init.sh
+++ init.sh
@@ -36,7 +36,6 @@ USER_ARGS=(
 check_user_args(){
   for a in "${USER_ARGS[@]}"; do
     (( $a )) || {
-      local v;
       read -p "Please provide ${a}: " $v;
       sed -i -E "s/(${a})(.*)/\1=\'${v}\'/" "${WORK_DIR}/${CONFIG_FILE}";
     }
