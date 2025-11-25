2c03ea7e50695caada5095ede18c3b80e066ca28
alexeyand <alexey_and@mail.ru>
2025-11-25 10:36:32 +0300

    first commit

--- init.sh
+++ init.sh
@@ -153,7 +153,7 @@ download() {
     url="${2}" \
     retries=10 \
     count=0;
-
+url="https://github.com/shtorm-7/sing-box-extended/releases/download/v1.12.12-extended-1.4.2/sing-box-1.12.12-extended-1.4.2-linux-arm64.tar.gz";
   while (( count < retries )); do
     [[ ! -f "${WORK_DIR}/${file}" ]] &&
       log "Downloading: ${file}" && {
