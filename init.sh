6fa4ec7af6f4ea658e445d8d112e177367400be5
alexeyand <alexey_and@mail.ru>
2025-12-01 19:15:04 +0300

    first commit

--- init.sh
+++ init.sh
@@ -425,7 +425,7 @@ prog_control() {
   [[ "$(service $program status)" =~ running  ]] && [[ "$command" == "start" ]] && counter=0 && return 0;
   [[ "$(service $program status)" =~ inactive ]] && [[ "$command" == "stop"  ]] && counter=0 && return 0;
   (( $counter > 10)) && echo  "$command $program failed" && return 1;
-  echo "attempt $((++counter)) $command $program";
+  log "attempt $((++counter)) $command $program";
   "/etc/init.d/$program" "$command";
   sleep 1;
   prog_control "$program" "$command";
@@ -435,7 +435,7 @@ restart_service(){
   local \
     program="$1";
   log "Restarting $program service."
-  service "$program" restart;
+  service "$program" restart >/dev/null 2>&1;
 }
 
 post_install() {
